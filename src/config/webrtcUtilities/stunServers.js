78e7310efd314a2b8a4c75ace0a33ea3e0443d5d
lenterer <rippersattack@gmail.com>
2025-07-01 10:52:48 +0700

    1/7

--- src/config/webrtcUtilities/stunServers.js
+++ src/config/webrtcUtilities/stunServers.js
@@ -1,12 +1,39 @@
-let peerConfiguration = {
-    iceServers:[
+const peerConfiguration = {
+    iceServers: [
+        // Objek pertama: berisi SEMUA server STUN
         {
-            urls:[
-              'stun:stun.l.google.com:19302',
-              'stun:stun1.l.google.com:19302'
+            urls: [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                // 'stun:stun2.l.google.com:19302',
+                // 'stun:stun3.l.google.com:19302',
+                // 'stun:stun4.l.google.com:19302',
+                // 'stun:stunserver.org'
             ]
-        }
-    ]
-}
+         }, // <-- Koma pemisah setelah objek STUN ditutup
 
-export default peerConfiguration
+        // Objek kedua: Konfigurasi TURN pertama
+        {
+            urls: "turn:global.relay.metered.ca:80",
+            username: "f00e8814970176f0f1c52730",
+            credential: "jMadhYKIv1AKUVqU",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:80?transport=tcp",
+            username: "f00e8814970176f0f1c52730",
+            credential: "jMadhYKIv1AKUVqU",
+        },
+        {
+            urls: "turn:global.relay.metered.ca:443",
+            username: "f00e8814970176f0f1c52730",
+            credential: "jMadhYKIv1AKUVqU",
+        },
+        {
+            urls: "turns:global.relay.metered.ca:443?transport=tcp",
+            username: "f00e8814970176f0f1c52730",
+            credential: "jMadhYKIv1AKUVqU",
+        },
+    ]
+};
+
+export default peerConfiguration;
